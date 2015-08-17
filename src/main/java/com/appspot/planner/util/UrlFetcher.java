175fe8cdbc6665de472e0c9a8f1fb7c29d9f9d58
Junyang Lu <junyangl@junyangl-macbookpro.roam.corp.google.com>
2015-08-16 18:14:37 -0700

    Use string join supported by java 7.

--- src/main/java/com/appspot/planner/util/UrlFetcher.java
+++ src/main/java/com/appspot/planner/util/UrlFetcher.java
@@ -1,12 +1,13 @@
 package com.appspot.planner.util;
 
+import com.google.common.base.Joiner;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.lang.String;
 import java.util.ArrayList;
 
 public class UrlFetcher {
@@ -23,7 +24,7 @@ public class UrlFetcher {
   public String getResult() {
     String result = "";
     try {
-      URL url = new URL(this.url + "?" + String.join("&", this.parameters));
+      URL url = new URL(this.url + "?" + Joiner.on("&").join(this.parameters));
       System.out.println(url.toString());
       BufferedReader reader =
           new BufferedReader(new InputStreamReader(url.openStream()));
